c23b8ca7081bc693fa19d31e6d1505a22a67b3d3
subhamondal123 <mondalsubha0@gmail.com>
2024-08-06 13:09:56 +0530

    sammm

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/youtube', (req,res) => {
     res.json('<h1>youtube2</h1>')
 })
 app.get('/sam', (req,res) => {
-    res.send('<h1>SAM</h1>')
+    res.send("sam")
 })
 app.listen(process.env.PORT, () => {
     console.log(`App listening to port ${port}`)
